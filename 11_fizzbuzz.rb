60b0bca544e5743e79618e348d45d8700f2443e4
vicnatacu <vicki@mvbmb.com>
2019-04-29 09:45:26 +1000

    challenge complete

--- 11_fizzbuzz.rb
+++ 11_fizzbuzz.rb
@@ -27,3 +27,17 @@
 # ...etc
 
 # Your code here
+
+(1..100).each do |n|
+    if n % 3 == 0 && n % 5 == 0
+        puts "fizzbuzz"
+    elsif   
+        n % 3 == 0
+        puts "fizz"
+    elsif
+        n % 5 == 0
+        puts "buzz"
+    else
+        puts n
+    end
+end
