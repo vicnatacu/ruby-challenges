7c0c596a56c81fc4b6350f1afe566b68dfc35cfa
vicnatacu <vicki@mvbmb.com>
2019-05-01 09:41:02 +1000

    updating multiplefiles

--- 11_fizzbuzz.rb
+++ 11_fizzbuzz.rb
@@ -41,3 +41,47 @@
         puts n
     end
 end
+
+number = 0
+while number < 100
+    number += 1
+    case
+    when (number % 3 == 0 && number % 5 == 0)
+        puts "fizzbuzz"
+    when (number % 3 == 0)
+        puts "fizz"
+    when (number % 5 == 0)
+        puts "buzz"
+    else
+        puts number
+    end
+end
+
+for i in 1..100
+    puts [i.to_s, (i % 3 == 0 ? "Fizz" : '') + (i % 5 == 0 ? "Buzz" : "")].max
+    
+    s = ""
+    if i % 3 == 0
+        s += "Fizz"
+    end
+    if 1 % 5 == 0
+        s += "Buzz"
+    end
+    puts [i.to_s, s].max
+end
+
+for i in 1..100
+    # second method using ternary and enumerable.max
+    puts [i.to_s, (i % 3 == 0 ? "Fizz" : '') + (i % 5 == 0 ? "Buzz" : '')].max
+
+    # below achieves does the same, but over mutliple lines for clarity
+
+    s = ""              # initialize an empty string
+    if i % 3 == 0       # if divisible by 3
+        s += "Fizz"     # then add "Fizz" to s
+    end
+    if i % 5 == 0       # if divisible by 5
+        s += "Buzz"     # then add "Buzz" to s
+    end
+    puts [i.to_s, s].max    # print the longest string, either the number or s
+end
--- 12_count_words.rb
+++ 12_count_words.rb
@@ -20,3 +20,60 @@
 # hi 3
 # hello 1
 # 1 2
+#creating hash
+def count_words(input)
+    # #testing
+    # puts input
+    input_arr = input.downcase.split(" ")
+    output = {}
+    # #testing
+    # puts input_arr 
+    input_arr.each  do |item|
+        if output_contains_key?(output, item)
+            output[item]+=1
+        else
+            output[item]=1
+        end
+        
+    end
+    output.each { |k, v| 
+     puts "#{k} #{v}"
+    }
+end
+    # hash iteration
+    def output_contains_key?(output, key)
+        output.each do |k, v|
+            if k == key
+                return true
+            end
+        end
+        return false
+    end
+
+ #main program starts here   
+puts "Please give me a phrase, saying or list of words."
+input=gets.chomp
+count_words(input)
+
+puts "Please give me a phrase, saying or list of words."
+input = gets.strip
+count_words(input)
+#count occurrence of each word in string
+#input is a string(phrase)
+#print list of words with occurence of each word(case insensitive)
+def count_words(phrase)
+    words_hash = Hash.new(0)
+    phrase.split(" ").each do |word|
+        words_hash[word.downcase]+=1
+    end
+    print_word_count(words_hash)
+    return words_hash
+end
+
+def print_word(word_hash)
+    words_hash.each do |key, value|
+        puts "#{key} #{value}"
+    end
+end
+
+
