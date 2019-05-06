d2407380d595c008d5ace8b5432c8f6e9c3cb85b
vicnatacu <vicki@mvbmb.com>
2019-05-07 09:10:24 +1000

    challenge complete

--- 15_change_letters.rb
+++ 15_change_letters.rb
@@ -19,3 +19,15 @@
 
 
 # Your code here
+def change_letters(input)
+    puts "Please type a word"
+    input = gets.strip.downcase.split(//)
+    input.each do |letter|
+        if letter == "s"
+            input[input.index(letter)] = "th"
+            
+        end
+    end
+    return input.join
+end
+puts change_letters("input")
