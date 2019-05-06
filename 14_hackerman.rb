cdca49d8011cfa5ac7db39099b09e77ba8fe06bd
vicnatacu <vicki@mvbmb.com>
2019-05-07 08:36:21 +1000

    challenge completed

--- 14_hackerman.rb
+++ 14_hackerman.rb
@@ -18,8 +18,37 @@
 
 
 # Your code here
+#collecting input in a secure way
+def collectinput
+    puts "Please type 3 or 4 words"
+    input = gets.strip
+    system("clear")
+    return input
+end
+    
+def protect_same(input_one, input_two)
+    #for comparing purposes put inputs into an array
+    input_one = input_one.downcase.split(" ")
+    input_two = input_two.downcase.split(" ")
 
+    #Initialize empty array for first input
+    output = []
 
+    #Iterate over input_one and compare to input_two
+    for word in input_one
+        if input_two.include?(word)
+            output << "HIDDEN"
+        else
+            output << word
+        end
+    end
+    return output.join(" ")
+end
+
+input_one = collectinput
+input_two = collectinput
+
+puts protect_same(input_one, input_two)
 
 # Beast mode:
 # Does your code deal with capital letters? Update your solution so that
